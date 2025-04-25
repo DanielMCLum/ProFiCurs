db8152169a67f256052f1ab47e10b2dbece22078
Daniel <danimclum@gmail.com>
2025-04-25 19:57:07 +0200

    creo la infraestuctura de playbook

--- 03-instances.tf
+++ 03-instances.tf
@@ -17,40 +17,6 @@ resource "aws_instance" "moodle" {
     image_id      = "ami-084568db4383264d4"
     instance_type = "t2.micro"
     key_name      = "Vokey"
-    user_data = <<-EOF
-        #!/bin/bash
-
-        # Actualizar los repositorios
-        sudo apt update -y
-
-        # Instalar Apache2, PHP y extensiones necesarias para Moodle
-        sudo apt install -y apache2 php libapache2-mod-php php-cli php-curl php-gd php-intl php-mbstring php-xml php-zip php-soap php-xmlrpc php-pgsql
-
-        # Descargar la última versión estable de Moodle
-        MOODLE_VERSION=$(wget -qO- https://download.moodle.org/latest.txt)
-        wget https://download.moodle.org/download.php/direct/$MOODLE_VERSION/moodle-$MOODLE_VERSION.tgz
-
-        # Crear el directorio de Moodle y descomprimir los archivos
-        sudo mkdir -p /var/www/html/moodle
-        sudo tar -xvzf moodle-$MOODLE_VERSION.tgz -C /var/www/html/
-        sudo mv /var/www/html/moodle/moodle/* /var/www/html/moodle/
-        sudo rm -rf /var/www/html/moodle/moodle/
-
-        # Crear el directorio de datos de Moodle
-        sudo mkdir -p /var/www/moodledata
-        sudo chown -R www-data:www-data /var/www/moodledata
-        sudo chmod -R 777 /var/www/moodledata
-
-        # Establecer la propiedad de los archivos de Moodle al usuario de Apache
-        sudo chown -R www-data:www-data /var/www/html/moodle
-
-        # Eliminar el archivo descargado
-        rm moodle-$MOODLE_VERSION.tgz
-
-        # Opcional: Reiniciar Apache2
-        sudo systemctl restart apache2
-
-    EOF
     
     vpc_security_group_ids = [aws_security_group.sg_moodle.id]
     tags = {
