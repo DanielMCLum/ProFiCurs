af388f3a81d18f8b2f1c3088394db7d2dfad15a0
dmoreno4 <danielmoreno664@gmail.com>
2025-05-06 16:57:45 +0200

    nuevoc

--- terraform-packer/provider.tf
+++ terraform-packer/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  subscription_id = "tu-id-de-suscripcion"  # Obligatorio
+  subscription_id = "9c83b4e6-cccc-4027-b0b1-3c6ff5807320"  # Obligatorio
   client_id       = "tu-client-id"          # Opcional si usas az login
   client_secret   = "tu-client-secret"      # Opcional si usas az login
-  tenant_id       = "tu-tenant-id"          # Opcional si usas az login
+  tenant_id       = "836f1d43-90b9-41eb-815f-6e37bd65ff30"  # Opcional si usas az login
 }
