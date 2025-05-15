b4c702ab86ccd8b579281262c62f2945457c1eff
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:15:27 +0200

    Update backend.tf

--- azure-wordpress-infra/backend.tf
+++ azure-wordpress-infra/backend.tf
@@ -1,10 +1,10 @@
+# Configuración del backend de Terraform en Azure Storage
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstate"
-    container_name       = "tfstate"
-    key                  = "azure-wordpress-infra.tfstate"
+    resource_group_name  = "terraform-state-rg"  # Grupo de recursos donde se almacena el estado
+    storage_account_name = "terraformstate"      # Cuenta de almacenamiento de Terraform
+    container_name       = "tfstate"             # Contenedor donde se guarda el archivo de estado
+    key                  = "azure-wordpress-infra.tfstate"  # Archivo que almacena el estado de la infraestructura
   }
 }
-
 
