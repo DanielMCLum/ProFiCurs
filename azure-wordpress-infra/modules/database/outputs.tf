c855f504d7163375dfdfb0345184a87b9aeeba35
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:25:05 +0200

    Update outputs.tf

--- azure-wordpress-infra/modules/database/outputs.tf
+++ azure-wordpress-infra/modules/database/outputs.tf
@@ -1,5 +1,5 @@
+# Output que devuelve el ID del servidor de base de datos en Azure
 output "db_id" {
-  description = "ID del servidor de base de datos"
+  description = "ID del servidor de base de datos, utilizado para la gestión y conexión con la aplicación."
   value       = azurerm_mysql_server.db.id
 }
-
