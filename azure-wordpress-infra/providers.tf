96390e14bc4da6d65645502da8bad03a2d52d610
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:16:39 +0200

    Update providers.tf

--- azure-wordpress-infra/providers.tf
+++ azure-wordpress-infra/providers.tf
@@ -1,14 +1,14 @@
+# Definición de proveedores requeridos para Terraform
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source  = "hashicorp/azurerm"  # Define el origen del proveedor
+      version = "~> 3.0"             # Especifica la versión compatible con Terraform
     }
   }
 }
 
+# Configuración del proveedor AzureRM para gestionar recursos en Azure
 provider "azurerm" {
-  features {}
+  features {}  # Habilita todas las características sin configuración adicional
 }
-
-
