1e87feeaa7020b295e2dd660ee1931199092bf85
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:22:52 +0200

    Update outputs.tf

--- azure-wordpress-infra/modules/compute/outputs.tf
+++ azure-wordpress-infra/modules/compute/outputs.tf
@@ -1,5 +1,5 @@
+# Output que devuelve el ID del VM Scale Set creado en Azure
 output "vmss_id" {
-  description = "ID del VM Scale Set"
+  description = "ID del VM Scale Set, utilizado para gestionar múltiples instancias de máquinas virtuales."
   value       = azurerm_virtual_machine_scale_set.vmss.id
 }
-
