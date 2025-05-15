9a68b09067f02ddba3a573f0d74b879eeab53ead
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:27:39 +0200

    Update outputs.tf

--- azure-wordpress-infra/modules/network/outputs.tf
+++ azure-wordpress-infra/modules/network/outputs.tf
@@ -1,15 +1,19 @@
+# Output que devuelve el ID de la VNet creada en Azure
 output "vnet_id" {
-  description = "ID de la VNet creada"
+  description = "ID de la VNet creada, utilizada para gestionar la infraestructura de red virtual."
   value       = azurerm_virtual_network.vnet.id
 }
 
+# Output que devuelve el ID de la subred pública dentro de la VNet
 output "public_subnet_id" {
-  description = "ID de la subred pública"
+  description = "ID de la subred pública, utilizada para el tráfico de red accesible externamente."
   value       = azurerm_subnet.public_subnet.id
 }
 
+# Output que devuelve el ID de la subred privada dentro de la VNet
 output "private_subnet_id" {
-  description = "ID de la subred privada"
+  description = "ID de la subred privada, utilizada para recursos internos con restricciones de acceso."
   value       = azurerm_subnet.private_subnet.id
 }
+
 
