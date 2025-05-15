9ecb39f984e806f61b4e72d25aeeb8b93b35b706
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:30:07 +0200

    Update outputs.tf

--- azure-wordpress-infra/modules/security/outputs.tf
+++ azure-wordpress-infra/modules/security/outputs.tf
@@ -1,5 +1,5 @@
+# Output que devuelve el ID del Network Security Group (NSG) en Azure
 output "nsg_id" {
-  description = "ID del Network Security Group"
+  description = "ID del Network Security Group, utilizado para gestionar reglas de acceso y seguridad en la red."
   value       = azurerm_network_security_group.nsg.id
 }
-
