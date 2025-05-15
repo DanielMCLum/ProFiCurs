e286c03b05e50ad46dbca98abc17c0324aeac823
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:28:47 +0200

    Update varaibles.tf

--- azure-wordpress-infra/modules/network/varaibles.tf
+++ azure-wordpress-infra/modules/network/varaibles.tf
@@ -1,23 +1,30 @@
+# Variables utilizadas para la configuración de la Virtual Network en Azure
+
+# Nombre asignado a la Virtual Network (VNet)
 variable "vnet_name" {
   description = "Nombre de la VNet"
   type        = string
 }
 
+# Ubicación donde se desplegará la VNet en Azure
 variable "location" {
   description = "Ubicación de la infraestructura"
   type        = string
 }
 
+# Grupo de recursos en Azure donde se creará la VNet
 variable "resource_group_name" {
   description = "Nombre del grupo de recursos"
   type        = string
 }
 
+# Espacio de direcciones IP asignado a la VNet
 variable "address_space" {
   description = "Espacio de direcciones IP para la VNet"
   type        = list(string)
 }
 
+# Configuración de la subred pública dentro de la VNet
 variable "public_subnet_name" {
   description = "Nombre de la subred pública"
   type        = string
@@ -28,6 +35,7 @@ variable "public_subnet_prefix" {
   type        = list(string)
 }
 
+# Configuración de la subred privada dentro de la VNet
 variable "private_subnet_name" {
   description = "Nombre de la subred privada"
   type        = string
@@ -37,4 +45,3 @@ variable "private_subnet_prefix" {
   description = "Prefijo de la subred privada"
   type        = list(string)
 }
-
