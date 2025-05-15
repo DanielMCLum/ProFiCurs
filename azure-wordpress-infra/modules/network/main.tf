991979587a90c9dfb4b8c0bbf4ed0b6c11e26542
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:27:17 +0200

    Update main.tf

--- azure-wordpress-infra/modules/network/main.tf
+++ azure-wordpress-infra/modules/network/main.tf
@@ -1,21 +1,23 @@
+# Creación de la Red Virtual (VNet) en Azure
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = var.address_space
+  name                = var.vnet_name  # Nombre de la VNet
+  location            = var.location  # Ubicación del recurso en Azure
+  resource_group_name = var.resource_group_name  # Grupo de recursos donde se aloja la red
+  address_space       = var.address_space  # Espacio de direcciones IP asignado
 }
 
+# Creación de la subred pública dentro de la VNet
 resource "azurerm_subnet" "public_subnet" {
-  name                 = var.public_subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.public_subnet_prefix
+  name                 = var.public_subnet_name  # Nombre de la subred pública
+  resource_group_name  = var.resource_group_name  # Grupo de recursos donde se aloja
+  virtual_network_name = azurerm_virtual_network.vnet.name  # Asociación con la VNet creada
+  address_prefixes     = var.public_subnet_prefix  # Prefijo de direcciones IP asignadas a la subred pública
 }
 
+# Creación de la subred privada dentro de la VNet
 resource "azurerm_subnet" "private_subnet" {
-  name                 = var.private_subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.private_subnet_prefix
+  name                 = var.private_subnet_name  # Nombre de la subred privada
+  resource_group_name  = var.resource_group_name  # Grupo de recursos donde se aloja
+  virtual_network_name = azurerm_virtual_network.vnet.name  # Asociación con la VNet creada
+  address_prefixes     = var.private_subnet_prefix  # Prefijo de direcciones IP asignadas a la subred privada
 }
-
