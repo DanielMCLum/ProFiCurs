b2574f83389ec3ea8b69004ba1d85449ce5900cf
dmoreno4 <150704742+dmoreno4@users.noreply.github.com>
2025-05-15 12:18:00 +0200

    Update variables.tf

--- azure-wordpress-infra/variables.tf
+++ azure-wordpress-infra/variables.tf
@@ -1,13 +1,17 @@
+# Variables utilizadas en Terraform para definir la infraestructura
+# Ubicación donde se desplegarán los recursos en Azure
 variable "location" {
   description = "Ubicación de la infraestructura"
   type        = string
 }
 
+# Nombre del grupo de recursos donde se gestionará la infraestructura
 variable "resource_group_name" {
   description = "Nombre del grupo de recursos"
   type        = string
 }
 
+# Credenciales del usuario administrador para gestionar las instancias de VM
 variable "admin_username" {
   description = "Usuario administrador"
   type        = string
@@ -18,11 +22,13 @@ variable "admin_password" {
   type        = string
 }
 
+# Tamaño de las máquinas virtuales a desplegar
 variable "vm_size" {
   description = "Tamaño de la máquina virtual"
   type        = string
 }
 
+# Cantidad de instancias de máquina virtual a aprovisionar
 variable "instance_count" {
   description = "Número inicial de instancias"
   type        = number
