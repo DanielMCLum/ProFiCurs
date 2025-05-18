3d8086615e5bf659bde1ab48d2dfb196d112ae27
ricardoramonpita <ricardo.ramonpita@gmail.com>
2025-05-19 01:37:50 +0200

    Filtra IP's Publicas para añadirlo al inventori.ini

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -5,28 +5,28 @@ ANSIBLE_DIR="ansible"
 INVENTORY_FILE="${ANSIBLE_DIR}/inventory.ini"
 GROUP_VARS_FILE="${ANSIBLE_DIR}/group_vars/all.yml"
 
-# Obtener IPs públicas desde Terraform
-IP_LIST=$(terraform -chdir=${TF_STATE_DIR} output -json | jq -r '.ec2_public_ips.value[]')
-
 # Obtener el ID del EFS desde Terraform
 EFS_ID=$(terraform -chdir=${TF_STATE_DIR} output -raw efs_id)
 
-# Crear/Actualizar archivo de inventario
-echo "[web]" > "${INVENTORY_FILE}"
-for ip in $IP_LIST; do
-  echo "$ip ansible_user=ubuntu ansible_ssh_private_key_file=terraform/devops.pem" >> "${INVENTORY_FILE}"
-done
-
-# Añadir el ID del EFS a group_vars/all.yml
+# Actualizar efs_id en group_vars/all.yml
 if grep -q "^efs_id:" "$GROUP_VARS_FILE"; then
-  # Si ya existe, lo reemplaza
   sed -i "s/^efs_id:.*/efs_id: ${EFS_ID}/" "$GROUP_VARS_FILE"
 else
-  # Si no existe, lo añade
   echo -e "\nefs_id: ${EFS_ID}" >> "$GROUP_VARS_FILE"
 fi
 
+# Obtener IPs públicas de instancias con el tag Name=wordpress-asg
+echo "[web]" > "$INVENTORY_FILE"
+aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=wordpress-asg" "Name=instance-state-name,Values=running" \
+  --query 'Reservations[*].Instances[*].PublicIpAddress' \
+  --output text |
+while read ip; do
+  echo "$ip ansible_user=ubuntu ansible_ssh_private_key_file=../terraform/devops.pem" >> "$INVENTORY_FILE"
+done
+
 echo "✅ Inventario actualizado en: ${INVENTORY_FILE}"
 echo "✅ EFS ID actualizado en: ${GROUP_VARS_FILE}"
+
 
 
