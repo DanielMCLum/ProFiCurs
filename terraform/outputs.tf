f1290861c13b6198cc54803d0bccfc8ed9c94ba7
ricardoramonpita <ricardo.ramonpita@gmail.com>
2025-05-19 01:43:34 +0200

    Actualizado output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,16 @@
+# Salida del DNS del Load Balancer
 output "alb_dns_name" {
   value = aws_lb.wordpress_alb.dns_name
 }
 
+# Salida del ID del sistema de archivos EFS
 output "efs_id" {
   value = aws_efs_file_system.wordpress.id
 }
 
+/*
 output "ec2_public_ips" {
   value = [for instance in aws_instance.wordpress : instance.public_ip]
 }
-
+*/
 
