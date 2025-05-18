67f796ceb04d32b891946d291494c17e73bbb1be
ricardoramonpita <ricardo.ramonpita@gmail.com>
2025-05-18 06:33:49 +0200

    Añadido output del efs_id

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,12 @@ output "alb_dns_name" {
   value = aws_lb.wordpress_alb.dns_name
 }
 
+output "efs_id" {
+  value = aws_efs_file_system.wordpress.id
+}
+
 output "ec2_public_ips" {
   value = [for instance in aws_instance.wordpress : instance.public_ip]
 }
+
 
