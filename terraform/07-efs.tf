2c684a8a3ed04df21a1932a20ddb7efc4fe7a02d
ricardoramonpita <ricardo.ramonpita@gmail.com>
2025-05-18 06:33:25 +0200

    Añadido Security Group para EFS al puerto 2049 (NFS)

--- terraform/07-efs.tf
+++ terraform/07-efs.tf
@@ -1,22 +1,45 @@
-# Crear sistema de archivos EFS
-resource "aws_efs_file_system" "wordpress_efs" {
+# Sistema de archivos EFS
+resource "aws_efs_file_system" "wordpress" {
   creation_token = "wordpress-efs"
-  performance_mode = "generalPurpose"
-  throughput_mode = "bursting"
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
   tags = {
     Name = "wordpress-efs"
   }
 }
 
-# Crear punto de montaje en cada subred (uno por AZ)
-resource "aws_efs_mount_target" "efs_mount" {
-  count          = length(data.aws_subnets.default.ids)
-  file_system_id = aws_efs_file_system.wordpress_efs.id
-  subnet_id      = data.aws_subnets.default.ids[count.index]
-  security_groups = [aws_security_group.wordpress_sg.id]
+# Security Group para permitir NFS desde las instancias EC2
+resource "aws_security_group" "efs_sg" {
+  name        = "efs-sg"
+  description = "Permitir NFS desde instancias EC2"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.wordpress_sg.id] # Desde instancias EC2
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "efs-sg"
+  }
 }
 
-# Exportar ID del EFS para Ansible
-output "efs_id" {
-  value = aws_efs_file_system.wordpress_efs.id
+# Mount targets en todas las subredes por defecto
+resource "aws_efs_mount_target" "wordpress" {
+  count           = length(data.aws_subnets.default.ids)
+  file_system_id  = aws_efs_file_system.wordpress.id
+  subnet_id       = data.aws_subnets.default.ids[count.index]
+  security_groups = [aws_security_group.efs_sg.id]
 }
+
