e6b3e6f0942a85b7741537bf2086ba1cb0b45f27
ricardoramonpita <ricardo.ramonpita@gmail.com>
2025-05-25 06:09:58 +0200

    Añadido output del endpoint del RDS para actualizar las variables globales

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,9 +8,14 @@ output "efs_id" {
   value = aws_efs_file_system.wordpress.id
 }
 
+output "rds_endpoint" {
+  value = aws_db_instance.wordpress.endpoint
+}
+
+
+
 /*
 output "ec2_public_ips" {
   value = [for instance in aws_instance.wordpress : instance.public_ip]
 }
 */
-
